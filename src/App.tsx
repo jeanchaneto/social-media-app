3c69e508518a73802e943f26d128d66be241eb45
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-11-03 22:20:37 +0100

    Router initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import Home from "./_root/pages/Home";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 function App() {
-
   return (
-    <>
-      <h1 className='' >Hello social</h1>
-    </>
-  )
+    <BrowserRouter>
+      <main className="flex h-screen">
+        <Routes>
+          {/* Public Routes */}
+          <Route element={<AuthLayout />}>
+            <Route path="/sign-in" element={<SigninForm />} />
+            <Route path="/sign-up" element={<SignUpForm />} />
+          </Route>
+          {/* Private Routes */}
+          <Route element={<RootLayout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
