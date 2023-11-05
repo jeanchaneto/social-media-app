b9dc69ad207dca8730897f542403b8c508f30ac4
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-11-05 17:45:00 +0100

    Post form 😅

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,13 +1,27 @@
 import * as z from "zod";
 
 export const SignupValidation = z.object({
-    name: z.string().min(2, {message: 'Too short'}),
-    username: z.string().min(2, {message: 'Too short'}),
-    email: z.string().email(),
-    password: z.string().min(8, {message: 'Pasword must be at least 8 characters'})
-  });
+  name: z.string().min(2, { message: "Too short" }),
+  username: z.string().min(2, { message: "Too short" }),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Pasword must be at least 8 characters" }),
+});
 
-  export const SigninValidation = z.object({
-    email: z.string().email(),
-    password: z.string().min(8, {message: 'Pasword must be at least 8 characters'})
-  });
+export const SigninValidation = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Pasword must be at least 8 characters" }),
+});
+
+export const PostValidation = z.object({
+  caption: z
+    .string()
+    .min(5, { message: "Caption must be at least 8 characters" })
+    .max(200, { message: "Caption must be less than 200 characters" }),
+    file: z.custom<File[]>(),
+    location: z.string().min(2).max(100),
+    tags: z.string()
+});
